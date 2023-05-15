225d6f86056429a4a22ab8e62c06053f677f2248
mauricio <britomauricio89@gmail.com>
2023-05-15 16:12:54 -0300

    projeto criado

---
+++ src/main/java/com/semana/intencivao2/Intencivao2Application.java
@@ -0,0 +1,13 @@
+package com.semana.intencivao2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intencivao2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Intencivao2Application.class, args);
+	}
+
+}
